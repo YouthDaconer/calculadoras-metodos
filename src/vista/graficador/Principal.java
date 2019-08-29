96f527d764657e6c944340feaf3c1881dcbb8fa3
Carlos Caicedo <carlos.andres.caicedo@hotmail.com>
2019-08-28 23:10:17 -0500

    Se añade la graficadora

---
+++ src/vista/graficador/Principal.java
@@ -0,0 +1,11 @@
+package vista.graficador;
+
+public class Principal
+{
+        public static void main(String args[])
+        {
+            // crea objeto JFrame
+            myFrame aplicacion = new myFrame();
+
+	} // fin de main 
+} // fin de la clase Pintor
