c2948067d459d7f2aedf1c488a3984bebc7b0ea4
JSRA <guapacha2@hotmail.com>
2019-08-29 19:10:54 -0500

    Clase que creará los modelos de tabla, falta desarrollo...

---
+++ src/modelo/ModelTabla.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author salas
+ */
+public class ModelTabla {
+    
+    public DefaultTableModel getModeloTabla(Object [] encabezados, Object[][] data){
+        DefaultTableModel dt = new DefaultTableModel();
+        return dt;
+    }
+    
+}
