3d5ee4ea9545044339efeb8c713325e11e523378
JSRA <guapacha2@hotmail.com>
2019-08-31 17:15:30 -0500

    Cambié ciertos parámetros para adecuarlo al estándar

--- src/control/ReglaFalsa.java
+++ src/control/ReglaFalsa.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package control;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,46 +10,80 @@ public class ReglaFalsa {
 
     //Atributos de clase
     private Funcion f;
-    private double err;
-    private int maxIteraciones, iteraciones;
+    private double a, b, errorTolerado, error;
+    private int iteraciones, maxIteraciones;
     private String cadena;
+    private Object[] encabezados = {"Iteración",
+        "Extremo\ninferior",
+        "Extremo\nsuperior",
+        "Imagen\nExt. inferior",
+        "Imagen\nExt. superior",
+        "Raiz",
+        "Imagen\nraiz",
+        "Error relativo"};
+    private ArrayList<Object[]> datos;
     //-----------------------------------
 
     //Constructores
-    public ReglaFalsa(Funcion f, double err, int maxIteraciones) {
+    public ReglaFalsa(Funcion f, double a, double b, double errorTolerado) {
         this.f = f;
-        this.err = err;
-        this.maxIteraciones = maxIteraciones;
+        this.a = a;
+        this.b = b;
+        this.errorTolerado = errorTolerado;
         this.iteraciones = 0;
-        this.cadena = "";
+        this.error = 0;
+        this.maxIteraciones = 200;
+        this.datos = new ArrayList<>();
     }
 
-    public ReglaFalsa(String expresion, double err, int maxIteraciones) {
-        this.f = new Funcion(expresion);
-        this.err = err;
+    public ReglaFalsa(String f, double a, double b, double errorTolerado) {
+        this.f = new Funcion(f);
+        this.a = a;
+        this.b = b;
+        this.errorTolerado = errorTolerado;
+        this.iteraciones = 0;
+        this.error = 0;
+        this.maxIteraciones = 200;
+        this.datos = new ArrayList<>();
+    }
+
+    public ReglaFalsa(Funcion f, double a, double b, double errorTolerado, int maxIteraciones) {
+        this.f = f;
+        this.a = a;
+        this.b = b;
+        this.iteraciones = 0;
+        this.error = 0;
+        this.errorTolerado = errorTolerado;
         this.maxIteraciones = maxIteraciones;
-        this.iteraciones = 0;
-        this.cadena = "";
-    }
-    
-    public ReglaFalsa(String expresion, double err) {
-        this.f = new Funcion(expresion);
-        this.err = err;
-        this.maxIteraciones = 100;
-        this.iteraciones = 0;
-        this.cadena = "";
-    }
-    
-    public ReglaFalsa(String expresion) {
-        this.f = new Funcion(expresion);
-        this.err = 1e-12;
-        this.maxIteraciones = 100;
-        this.iteraciones = 0;
-        this.cadena = "";
+        this.datos = new ArrayList<>();
     }
     //----------------------------------------------
 
-    //Métodos get and set
+    //Getters and Setters
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getErrorTolerado() {
+        return errorTolerado;
+    }
+
+    public double getError() {
+        return error;
+    }
+
+    public int getIteraciones() {
+        return iteraciones;
+    }
+
+    public int getMaxIteraciones() {
+        return maxIteraciones;
+    }
+
     public String getF() {
         return f.getFuncion();
     }
@@ -63,66 +92,68 @@ public class ReglaFalsa {
         this.f = new Funcion(f);
     }
 
-    public void setErr(double err) {
-        this.err = err;
+    public void setA(double a) {
+        this.a = a;
     }
 
-    public int getIteraciones() {
-        return iteraciones;
+    public void setB(double b) {
+        this.b = b;
     }
 
-    public String getProceso() {
-        return cadena;
+    public void setErrorTolerado(double errorTolerado) {
+        this.errorTolerado = errorTolerado;
     }
-    //--------------------------------        
+
+    public void setMaxIteraciones(int maxIteraciones) {
+        this.maxIteraciones = maxIteraciones;
+    }
+
+    public Object[] getEncabezados() {
+        return encabezados;
+    }
+
+    public ArrayList<Object[]> getDatos() {
+        return datos;
+    }
+    //--------------------------------------   
 
     //Método que da la solución
-    public String resolver(double x1, double x2) {
+    public String resolver() {
 
         int numDec = 5;
-
+        double x1=a;
+        double x2 = b;
         double x3, y3;
         double xp = x1;
         double y1 = f.f(x1);
         double y2 = f.f(x2);
 
-        //Valores iniciales
-        cadena += (" x1 = " + round(x1, numDec) + "\n");
-        cadena += (" x2 = " + round(x2, numDec)) + "\n";
-        cadena += (" y1: f.f(" + x1 + ")= " + round(y1, numDec) + "\n");
-        cadena += (" y2: f.f(" + x2 + ")= " + round(y2, numDec) + "\n");
+        
 
         for (int i = 1; i <= maxIteraciones; i++) {
-            cadena += ("Iteración #" + i + "\n");
+            
             if ((y2 - y1) == 0) {
-                cadena += ("Error: no converge x=NaN; iteración=" + i + "\n");
+                
                 return "NaN";
             }
 
             x3 = (y2 * x1 - y1 * x2) / (y2 - y1);
-            cadena += (" x3 = (y2*x1-y1*x2)/(y2-y1)" + "\n");
-            cadena += (" x3 = ((" + round(y2, numDec) + ")*(" + round(x1, numDec) + ")-(" + round(y1, numDec) + ")*(" + round(x2, numDec) + "))/((" + round(y2, numDec) + ")-(" + round(y1, numDec) + ")) = " + round(x3, numDec) + "\n");
-
-            cadena += ("Error |" + round(xp, numDec) + "/" + round(x3, numDec) + "-1|<" + round(err, numDec) + "  " + (Math.abs(xp / x3 - 1) < err) + "\n");
-            if (Math.abs(xp / x3 - 1) < err) {
-                cadena += (round(Math.abs(xp / x3 - 1), numDec) + "<" + round(err, numDec) + " -> termina programa" + "\n");
+            
+            if (Math.abs(xp / x3 - 1) < errorTolerado) {
+                
                 iteraciones = i;
                 return String.valueOf(x3);
             }
 
             y3 = f.f(x3);
 
-            cadena += (" y1 = " + round(y1, numDec) + "\n");
-            cadena += (" y3 = fx(x3) = fx(" + round(x3, numDec) + ") = " + round(y3, numDec) + "\n");
-            cadena += (" y1*y3 < 0 " + (y1 * y3 < 0) + "\n");
+           
             if (y1 * y3 < 0) {
-                cadena += ("   x2=x3=" + round(x3, numDec) + "\n");
-                cadena += ("   y2=y3=" + round(y3, numDec) + "\n");
+                
                 x2 = x3;
                 y2 = y3;
             } else {
-                cadena += ("   x1=x3=" + round(x3, numDec) + "\n");
-                cadena += ("   y1=y3=" + round(y3, numDec) + "\n");
+                
                 x1 = x3;
                 y1 = y3;
             }
@@ -131,12 +162,6 @@ public class ReglaFalsa {
         }
         return "" + xp;
 
-    }
-
-    private String round(double num, int numDec) {
-        BigDecimal bd = new BigDecimal(num);
-        bd = bd.setScale(numDec, BigDecimal.ROUND_HALF_UP);
-        return bd.toString();
     }
 
     //pruebas
