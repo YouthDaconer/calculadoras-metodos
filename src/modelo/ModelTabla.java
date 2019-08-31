889a1b53e90f3c020ed1222e783b1a9136ae9c14
JSRA <guapacha2@hotmail.com>
2019-08-31 16:45:41 -0500

    Añadí un constructor y los atributos respectivos para que la clase sea más dinámica

--- src/modelo/ModelTabla.java
+++ src/modelo/ModelTabla.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
 import java.util.ArrayList;
@@ -13,9 +8,29 @@ import javax.swing.table.DefaultTableModel;
  * @author salas
  */
 public class ModelTabla {
-    
-    //Método que devuelve un modelo de tabla
-    public DefaultTableModel getModeloTabla(Object [] encabezados, ArrayList<Object[]> datos){
+
+    //Atributos de clase
+    private Object[] encabezados;
+    private ArrayList<Object[]> datos;
+    //-------------------------------
+
+    /**
+     * constructor
+     *
+     * @param encabezados
+     * @param datos
+     */
+    public ModelTabla(Object[] encabezados, ArrayList<Object[]> datos) {
+        this.encabezados = encabezados;
+        this.datos = datos;
+    }
+    //----------------------------
+
+    /**
+     * 
+     * @return modelo de tabla
+     */
+    public DefaultTableModel getModeloTabla() {
         DefaultTableModel dt = new DefaultTableModel(); //Creo el objeto  
         dt.setColumnIdentifiers(encabezados);  //pongo los encabezados
         datos.forEach((fila) -> {//recorro la lista
